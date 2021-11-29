a3e1e2c5aa62e591676fdce0ed93270494608743
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-29 13:50:11 +0100

    character + JSON

--- lib/model/character.dart
+++ lib/model/character.dart
@@ -5,4 +5,15 @@ class Character{
   late Map<String, dynamic> info;
   List<Result>results = [];
 
+  Character.json(Map<String, dynamic> map){
+    Result ?results;
+    results!.id = map["id"];
+    results.name = map["name"];
+    results.status = map["status"];
+    results.species = map["species"];
+    results.type = map["type"];
+    results.gender = map["gender"];
+
+  }
+
 }
