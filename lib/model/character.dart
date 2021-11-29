cf7040051ca719f71cb1a97fa6d67ed180979b1d
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-29 12:09:40 +0100

    Initial commit

---
+++ lib/model/character.dart
@@ -0,0 +1,8 @@
+import 'package:rick_et_morty/model/result.dart';
+
+class Character{
+
+  late Map<String, dynamic> info;
+  List<Result>results = [];
+
+}
---
+++ lib/model/result.dart
@@ -0,0 +1,14 @@
+class Result{
+  late int id;
+  late String name;
+  late String status;
+  late String species;
+  late String type;
+  late String gender;
+
+  late Map<String, dynamic>origin;
+
+  late String image;
+  late String url;
+  late String created;
+}
